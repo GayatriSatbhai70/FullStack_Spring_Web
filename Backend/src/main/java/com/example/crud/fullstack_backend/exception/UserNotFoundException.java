96bf8c7a23165a8bf9805c63f8e51b8466223ac9
Gayatri Pandharinath Satbhai <gayatrisatbhai49@gmail.com>
2025-12-24 09:37:52 +0530

    Add files via upload

---
+++ Backend/src/main/java/com/example/crud/fullstack_backend/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.example.crud.fullstack_backend.exception;
+
+public class UserNotFoundException extends RuntimeException {
+public UserNotFoundException(Long id) {
+	super("Could not find this ID"+id);
+}
+}
